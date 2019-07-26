b74fd97cec4947e524bc47056962778120d86801
Anan Roderick <thamarnan@gmail.com>
2019-07-26 13:13:25 -0700

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -1,3 +1,4 @@
+# brute force
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -10,3 +11,21 @@ class Solution(object):
                 if nums[i] + nums[j] == target:
                     return [i,j]
         
+
+        
+        
+        
+# one pass
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i in range(len(nums)):
+            if target-nums[i] not in dict:
+                dict[nums[i]]=i
+            else:
+                return [dict[target-nums[i]],i]
